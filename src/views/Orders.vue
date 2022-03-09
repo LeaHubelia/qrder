560035cb0464dea6fcf3936ca466fe1f4634cd37
saif <saif.trabelsi.perso@gmail.com>
2022-03-09 17:26:36 -0500

    Add elements to Orders

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -4,26 +4,42 @@
       <v-list-item v-for="(o, index) in orders" :key="index">
         <v-list-item-content class="menu">
             <v-row>
-              <v-col>
+              <v-col class="image">
                 <v-img width="100%" height="100%" :src="o.img"></v-img>
               </v-col>
               <v-col>
                 <v-row>
-                  <v-col class="">
-                    <h4 class="items">
+                  <v-col>
+                    <v-list-item-title>
                       {{ o.restaurantName }}
-                    </h4>
-                  </v-col>
-                  <v-col  align="right">
-                    <h5 style="margin-top: 10px">{{ o.restaurantName }}
-                    </h5>
+                    </v-list-item-title>
+                  </v-col>                
+                </v-row>
+                <v-row>
+                  <v-col md="8" sm="8">
+                    <v-list-item-subtitle>
+                      {{ o.description }}
+                    </v-list-item-subtitle>
                   </v-col>
                 </v-row>
                 <v-row>
-                  <v-list-item-subtitle class="description">{{
-                    o.description
-                  }}</v-list-item-subtitle>
+                  <v-col>
+                    <v-list-item-subtitle>
+                      {{ o.date }} - {{ o.total }} $ CA
+                    </v-list-item-subtitle>
+                  </v-col>
                 </v-row>
+              </v-col>
+              <v-col align-self="center">
+                <div class="text-center align">
+                  <v-btn
+                    rounded
+                    color="normal"
+                    dark
+                  >
+                    Details
+                  </v-btn>
+                </div>
               </v-col>
             </v-row>
             <v-divider />
@@ -75,4 +91,8 @@ h3 {
 .description {
   margin-bottom:20px;
 }
+.image {
+  padding: 2%;
+}
+
 </style>
